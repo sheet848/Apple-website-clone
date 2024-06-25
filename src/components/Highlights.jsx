2f5178ea3265e48f0e899d180a95825fe7d11fa1
sheet848 <160768361+sheet848@users.noreply.github.com>
2024-06-25 22:53:20 +0530

    project code files

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Highlights = () => {
+  return (
+    <>
+    <section className='w-screen overflow-hidden h-full common-padding bg-zinc-500'>
+        <div className='screen-max-width'>
+            <h1 id='title' className='section-heading'>Get the Highlights.</h1>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Highlights
